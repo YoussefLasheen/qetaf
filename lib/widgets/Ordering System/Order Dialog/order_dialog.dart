49122846a031d089f50e75db04f266912b48dfa7
Youssef Lasheen <youssef@lasheen.dev>
2021-12-05 22:12:45 +0200

    Add OrderConfirmationDialog to show when order is done.

--- lib/widgets/Ordering System/Order Dialog/order_dialog.dart
+++ lib/widgets/Ordering System/Order Dialog/order_dialog.dart
@@ -372,3 +372,49 @@ class SidePanel extends StatelessWidget {
     }
   }
 }
+
+class OrderConfirmationDialog extends StatefulWidget {
+  final OrderingProcessModel process;
+  OrderConfirmationDialog({Key? key, required this.process,}) : super(key: key);
+
+  @override
+  _OrderConfirmationDialogState createState() =>
+      _OrderConfirmationDialogState();
+}
+
+class _OrderConfirmationDialogState extends State<OrderConfirmationDialog> {
+  bool _loading = true;
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      _loading = false;
+      setState(() {});
+    });
+    return Card(
+      child: _loading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Success",),
+                    Icon(Icons.done_rounded,color: Colors.green,),
+                  ],
+                ),
+                Text('Order Number: 1153364788'),
+                Spacer(),
+                TextButton(onPressed: (){
+                      Navigator.pop(context);
+                      widget.process.reset();
+                    }, child: Text('Close'))
+              ],
+            ),
+          ),
+    );
+  }
+}
