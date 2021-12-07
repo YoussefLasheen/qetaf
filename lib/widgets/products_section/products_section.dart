d3970f260d0e275063b1da5c8880079dc1b536d4
Youssef Lasheen <youssef@lasheen.dev>
2021-12-07 18:57:07 +0200

    Replace expand widget with an animated builder for better customizability.

--- lib/widgets/products_section/products_section.dart
+++ lib/widgets/products_section/products_section.dart
@@ -1,66 +1,93 @@
 import 'dart:ui';
 
-import 'package:expand_widget/expand_widget.dart';
 import 'package:flutter/material.dart';
 import 'widgets/product_card.dart';
 
 import 'models/product.dart';
 
-class ProductsSection extends StatelessWidget {
+class ProductsSection extends StatefulWidget {
   const ProductsSection({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<ProductsSection> createState() => _ProductsSectionState();
+}
 
-    return Row(
-      children: [
-        const Spacer(),
-        Expanded(
-          flex: 4,
-          child: Column(
+class _ProductsSectionState extends State<ProductsSection>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      _isExpanded ? _controller.forward() : _controller.reverse();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: _controller.view,
+        builder: (BuildContext context, Widget? child) {
+          return Column(
             children: [
-              const SizedBox(height: 100,),
-              const Text(
-                "منـــتجاتنا",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                "عسل نحل طبيعي ١٠٠٪ من مزارعنا في الغربية",
-                style: TextStyle(
-                    fontWeight: FontWeight.w300, color: Colors.black38),
-              ),
+              const SizedBox(height: 90,),
               Column(
                 children: [
-                  const SizedBox(height: 100,),
-                  Wrap(
-                    spacing: 100,
-                    textDirection: TextDirection.rtl,
-                    children: [
-                      ...List.generate(3, (index) {
-                        return ProductCard(
-                            product: demoProducts[index]);
-                      }),
-                    ],
+                  const Text(
+                    "منـــتجاتنا",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                   ),
-                  ExpandChild(
+                  const Text(
+                    "عسل نحل طبيعي ١٠٠٪ من مزارعنا في الغربية",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300, color: Colors.black38),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 100,),
+              ClipRect(
+                child: Align(
+                  heightFactor: (_controller
+                      .drive(CurveTween(curve: Curves.fastOutSlowIn))
+                      .value+0.5).clamp(0.5, 1),
+                  alignment: AlignmentDirectional.topCenter,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 1200),
                     child: Wrap(
                       spacing: 100,
-                      textDirection: TextDirection.rtl,
                       children: [
-                        ...List.generate(demoProducts.length -3, (index) {
-                        return ProductCard(
-                            product: demoProducts.sublist(3, demoProducts.length)[index]);
-                      }),
+                        ...List.generate(demoProducts.length, (index) {
+                          return ProductCard(product: demoProducts[index]);
+                        }),
                       ],
                     ),
                   ),
-                ],
+                ),
+              ),
+              IconButton(
+                icon: _isExpanded? Icon(Icons.keyboard_arrow_up_rounded):Icon(Icons.keyboard_arrow_down_rounded),
+                onPressed: () => _toggleExpand(),
               )
             ],
-          ),
-        ),
-        const Spacer()
-      ],
-    );
+          );
+        });
   }
 }
